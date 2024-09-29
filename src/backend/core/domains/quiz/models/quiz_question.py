a41593f960ae789ff11cf717798d0d9750678aa7
Khanh Nguyen <knguyen.ct@gmail.com>
2024-09-29 21:17:18 +0700

    update quiz question retrival

--- src/backend/core/domains/quiz/models/quiz_question.py
+++ src/backend/core/domains/quiz/models/quiz_question.py
@@ -1,8 +1,9 @@
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.dialects.postgresql import JSONB
-from sqlalchemy.orm import relationship
 
 from core.db.database import Base
+
+from .quiz import Quiz
 
 
 class QuizQuestion(Base):
@@ -10,7 +11,7 @@ class QuizQuestion(Base):
 
     id = Column(Integer, primary_key=True)
     content = Column(String)
-    quiz_id = ForeignKey('Quiz.id')
+    quiz_id = Column(Integer, ForeignKey(Quiz.id))
     score = Column(Integer, default=1)
     expected_option = Column(String, nullable=True)
     # Structure of options: [{'code': 'A', 'content': 'content of option A'}, ...]
