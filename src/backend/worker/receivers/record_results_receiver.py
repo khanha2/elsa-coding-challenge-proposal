290586e2b1d28c60b832e0b43271d1d4f27c4ba9
Khanh Nguyen <knguyen.ct@gmail.com>
2024-09-30 05:42:23 +0700

    add record result receiver

--- src/backend/worker/receivers/record_results_receiver.py
+++ src/backend/worker/receivers/record_results_receiver.py
@@ -1,4 +1,6 @@
-from core.services.rabbitmq import connection
+import json
+
+from core.services.rabbitmq import connection, sender
 from core.domains.quiz.actions import write_participant
 
 
@@ -7,6 +9,7 @@ def start():
 
     def callback(ch, method, properties, body):
         write_participant(body)
+        sender.send_message('message_broadcasting', json.loads(body))
 
     channel.basic_consume(
         queue='record_results', on_message_callback=callback, auto_ack=True)
